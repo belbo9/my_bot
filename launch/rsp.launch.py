387b2d882b58228bde5027dc34550f300662d8d8
belbo9 <124001900+belbo9@users.noreply.github.com>
2024-12-01 11:57:44 +0000

    h

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -12,7 +12,7 @@ import xacro
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
+    # Check if we're told to use sim time  
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Process the URDF file
